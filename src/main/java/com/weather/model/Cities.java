86e3b4d6ce3a0dbb7d40272b90f9062db2b43716
saurabhamin <saurabhamin@192.168.0.100>
2019-06-05 00:01:24 +0530

    added jackson api for json parser

---
+++ src/main/java/com/weather/model/Cities.java
@@ -0,0 +1,21 @@
+package com.weather.model;
+
+public class Cities {
+
+	private String cityName;
+	private Double cityId;
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public Double getCityId() {
+		return cityId;
+	}
+	public void setCityId(Double cityId) {
+		this.cityId = cityId;
+	}
+	
+	
+}
--- src/main/java/com/weather/runner/WeatherServiceRest.java
+++ src/main/java/com/weather/runner/WeatherServiceRest.java
@@ -3,12 +3,16 @@ package com.weather.runner;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.weather.model.Cities;
 
 @RestController
 public class WeatherServiceRest {
@@ -19,26 +23,23 @@ public class WeatherServiceRest {
     }
     
     
-    @PostMapping("/uploadFile")
-    public String uploadFile(@RequestParam("file") MultipartFile file) {
+    @PostMapping("/uploadCities")
+    public String uploadCities(@RequestParam("file") MultipartFile file) {
     	
     	   if (!file.isEmpty()) { 
     	        try { 
     	            byte[] bytes = file.getBytes();     
     	            
     	            String content = new String(bytes);
+    	            	
+    	            ObjectMapper mapper = new ObjectMapper();
     	            
-    	            // Creating the directory to store file 
-//    	            String rootPath = System.getProperty("catalina.home"); 
-//    	            File dir = new File(rootPath + File.separator + "tmpFiles"); 
-//    	            if (!dir.exists()) 
-//    	                dir.mkdirs();     
-//    	            // Create the file on server 
-//    	            File serverFile = new File(dir.getAbsolutePath() + File.separator + "_1"); 
-//    	            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile)); 
-//    	            stream.write(bytes);
-//    	            stream.close(); 
-//    	            System.out.println("Server File Location=" + serverFile.getAbsolutePath());
+    	            // convert JSON string to Map
+    	            Map<String, Integer> map = mapper.readValue(content, Map.class);
+    	            
+    	            map.forEach((cities,ids) -> {
+    	            	System.out.println(cities + " = "+ ids );
+    	            });
     	            return content; 
     	        } catch (Exception e) { 
     	            return null; 
