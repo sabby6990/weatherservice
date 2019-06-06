09c1e3564125e3996d5a4def055fa6a836c80604
saurabh <saurabh.amin@freestone-infotech.com>
2019-06-06 18:23:27 +0530

    started test cases for util and service

--- src/test/java/com/weather/runner/OpenWeatherAppApplicationTests.java
+++ src/test/java/com/weather/runner/OpenWeatherAppApplicationTests.java
@@ -1,16 +1,30 @@
 package com.weather.runner;
 
+import java.io.IOException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.weather.util.FileUtil;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OpenWeatherAppApplicationTests {
 
+	@Mock
+	FileUtil fileUtil;
+	
 	@Test
 	public void contextLoads() {
+		System.out.println("this is a test unit test");
+	}
+	
+	@Test
+	public void testSendGet() throws IOException{
+
 	}
 
 }
